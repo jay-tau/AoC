a3514014374e0ed77abe536dce6bc4fd371eddf3
Joel Tony <joeltony72@gmail.com>
2022-12-14 21:56:07 +0530

    Add 2022-02: 2

--- 2022/02/2022_02.py
+++ 2022/02/2022_02.py
@@ -2,23 +2,30 @@ score = 0
 with open("input.txt") as file:
     for line in file:
         opp_move = line[0]
-        my_move = line[2]
-        if my_move == "X":
-            score += 1
-            if opp_move == "C":
-                score += 6
-            elif opp_move == "A":
-                score += 3
-        elif my_move == "Y":
-            score += 2
+        direction = line[2]
+        if direction == "X":
+            score += 0
             if opp_move == "A":
-                score += 6
+                score += 3  # Z
             elif opp_move == "B":
-                score += 3
-        elif my_move == "Z":
-            score += 3
-            if opp_move == "B":
-                score += 6
+                score += 1  # X
             elif opp_move == "C":
-                score += 3
+                score += 2  # Y
+        elif direction == "Y":
+            score += 3
+            if opp_move == "A":
+                score += 1  # X
+            elif opp_move == "B":
+                score += 2  # Y
+            elif opp_move == "C":
+                score += 3  # Z
+        elif direction == "Z":
+            score += 6
+            if opp_move == "A":
+                score += 2  # Y
+            elif opp_move == "B":
+                score += 3  # Z
+            elif opp_move == "C":
+                score += 1  # X
+
 print(score)
