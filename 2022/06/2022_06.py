5e7a26d3538072931a40383b214e870b167c1895
Joel Tony <joeltony72@gmail.com>
2023-01-07 20:46:42 +0530

    Day 06: Part 2

--- 2022/06/2022_06.py
+++ 2022/06/2022_06.py
@@ -2,8 +2,8 @@ import collections
 
 with open("input.txt") as file:
     data = file.read()
-    for i in range(len(data) - 4):
-        count_dict = collections.Counter(data[i : i + 4])
-        if len(count_dict) >= 4:
-            print(i + 4)
+    for i in range(len(data) - 14):
+        count_dict = collections.Counter(data[i : i + 14])
+        if len(count_dict) >= 14:
+            print(i + 14)
             break
